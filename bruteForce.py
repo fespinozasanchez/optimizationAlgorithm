97611b83ad11f85cf0f12ff6f0895ca19c687111
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2022-04-28 23:13:03 -0400

    Add functionObjetive

--- bruteForce.py
+++ bruteForce.py
@@ -5,18 +5,21 @@ x = x1 = x2 = 0
 thetaV = 0.0
 deltaXV = 0.0
 
-
+def functionObjective(x1, x2, thetaV):
+    return 1.20 * x1 + 1.16 * x2 - thetaV *  (2*p(x1, 2) + p(x2, 2)+p((x1+x2), 2))
+    
 def bruteForce(thetaV, deltaXV, list=[]):
     try:
         for x1 in np.arange(0, 6, deltaXV):
             for x2 in np.arange(0, 6, deltaXV):
                 if((x1 + x2) <= 5):
-                    evA = 1.20 * x1 + 1.16 * x2 - thetaV *  (2*p(x1, 2) + p(x2, 2)+p((x1+x2), 2))
+                    evA = functionObjective(x1,x2,thetaV)
                 x = (x1, x2)
                 list.append((x, evA))
     except Exception as e:
         print(e)
     return list
+
 
 def maxTupla(arr):
     if len(arr)!=0: 
