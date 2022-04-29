f4927e886681538aada9d05d4b63a1d1534d1552
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2022-04-29 17:00:37 -0400

    update bruteForce

--- bruteForce.py
+++ bruteForce.py
@@ -1,20 +1,21 @@
 from math import pow as p
 import numpy as np
 
-# def objectiveFunction(x1, x2, thetaV):
-#     """
+x = x1 = x2 = 0
+thetaV = 0.0
+deltaXV = 0.0
 
-#     :param x1:Expect the x1 value for the objective function.
-#     :param x2:Expect the x2 value for the objective function.
-#     :param thetaV:Expect the θ value for the objective function , is a constant that represents the risk .
-#     :return:The function evaluated ,Z Value.
-#     """
-#     return 1.20 * x1 + 1.16 * x2 - thetaV *  (2*p(x1, 2) + p(x2, 2)+p((x1+x2), 2))
+def objectiveFunction(x1, x2, thetaV):
+    """
 
-def objectiveFunction2(functionObejctive):
-    return functionObejctive
-
-def bruteForce(*args, list=[]):
+    :param x1:Expect the x1 value for the objective function.
+    :param x2:Expect the x2 value for the objective function.
+    :param thetaV:Expect the θ value for the objective function , is a constant that represents the risk .
+    :return:The function evaluated ,Z Value.
+    """
+    return 1.20 * x1 + 1.16 * x2 - thetaV *  (2*p(x1, 2) + p(x2, 2)+p((x1+x2), 2))
+    
+def bruteForce(thetaV, deltaXV, list=[]):
     """
     This function applies brute force to find all suboptimal solutions given a restriction and save the points evaluated and its value Z.
     :param thetaV:Refers to the θ value of this problem to evaluate.
@@ -23,10 +24,10 @@ def bruteForce(*args, list=[]):
     :return: The list generated.
     """
     try:
-        for x1 in np.arange(0, 6, args[3]):
-            for x2 in np.arange(0, 6, args[3]):
+        for x1 in np.arange(0, 6, deltaXV):
+            for x2 in np.arange(0, 6, deltaXV):
                 if((x1 + x2) <= 5):
-                    evA = objectiveFunction2(args[0] * x1 + args[2] * x2 - args[2] *  (2*p(x1, 2) + p(x2, 2)+p((x1+x2), 2)))
+                    evA = objectiveFunction(x1,x2,thetaV)
                 x = (x1, x2)
                 list.append((x, evA))
     except Exception as e:
@@ -44,15 +45,10 @@ def maxTupla(arr):
         return max(arr, key=lambda x: x[1])
     return "Empty Array"
 
-def main():    
-    x = x1 = x2 = 0
-    thetaV = 0.0
-    deltaXV = 0.0
-    A = 1.20
-    B = 1.16
+def main():
     thetaV = float(input("ThetaV: "))
     deltaXV = float(input("DeltaXV: "))
-    list = bruteForce(A,B,thetaV, deltaXV)
+    list = bruteForce(thetaV, deltaXV)
     print(maxTupla(list))
 
 if __name__ == '__main__':
