bc5ff4907b7056e3d59f7148a12c8f103131c5cb
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2022-04-28 22:50:25 -0400

    update dode

--- bruteForce.py
+++ bruteForce.py
@@ -1,16 +1,34 @@
-import  math
+from math import pow as p
 import numpy as np
-x=x1=x2=0
-theta=0.0
-deltaX =0.0
-xyArray =[]
-def isEmpty(arr):
-    return len(arr) == 0
 
-def bruteForce(thetaV, deltaXV):
-    for x1 in np.arange(0, 6, deltaX):
-        for x2 in np.arange(0, 6, deltaX):
-            if((x1 + x2) <=5):
-                objF= 1.20 * x1 + 1.16 * x2 - theta * (2*math.pow(x1,2)+ math.pow(x2,2)+math.pow((x1+x2),2))
-            x=(x1,x2)
-            xyArray.append((x,objF))
+x = x1 = x2 = 0
+thetaV = 0.0
+deltaXV = 0.0
+
+
+def bruteForce(thetaV, deltaXV, list=[]):
+    try:
+        for x1 in np.arange(0, 6, deltaXV):
+            for x2 in np.arange(0, 6, deltaXV):
+                if((x1 + x2) <= 5):
+                    evA = 1.20 * x1 + 1.16 * x2 - thetaV *  (2*p(x1, 2) + p(x2, 2)+p((x1+x2), 2))
+                x = (x1, x2)
+                list.append((x, evA))
+    except Exception as e:
+        print(e)
+    return list
+
+def maxTupla(arr):
+    if len(arr)!=0: 
+        return max(arr, key=lambda x: x[1])
+    return "Empty Array"
+
+def main():
+    thetaV = float(input("ThetaV: "))
+    deltaXV = float(input("DeltaXV: "))
+    list = bruteForce(thetaV, deltaXV)
+    print(maxTupla(list))
+
+if __name__ == '__main__':
+    main()
+
