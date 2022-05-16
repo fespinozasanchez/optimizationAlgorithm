029f401b14d3093c0c0fdae6ca163e2f49fcf0f4
Mario <mariocastillosan@gmail.com>
2022-05-15 20:24:12 -0400

    ahora si que si

--- Numerical Derivative/main.py
+++ Numerical Derivative/main.py
@@ -4,7 +4,6 @@ from math import sqrt
 def f(x, str):
     """
     It takes a string and evaluates it as a Python expression
-
     :param str: The string to evaluate
     """
     return eval(str)
@@ -25,7 +24,6 @@ def derive(x_symbol, fx):
 def algorithm_1(fx, x):
     """
     It takes a function and a value, and returns the derivative of the function at that value
-
     :param fx: The function to be derived
     :param x: The value of x that you want to use in the function
     """
@@ -34,36 +32,33 @@ def algorithm_1(fx, x):
     return eval(str(derivation))
 
 
-def algorithm_2(dfx, x, dx, f, str,e):
+def algorithm_2(dfx, x, dx, f, str):
     """
     It takes in a function, a value, a step size, and a string, and returns the difference between the
     derivative of the function at the value and the limit of the function at the value
-    
     :param dfx: the derivative of the function f(x)
     :param x: the value of x that you want to find the derivative at
-    :param dx: the step size
+    :param dx: delta x value
     :param f: function
     :param str: string of the function
-    :param e: the error tolerance
     :return: The difference between the actual derivative and the derivative calculated by the
     algorithm.
     """
     lim = (f(x+dx,str)- f(x,str))/dx
-    f_x_d = abs(dfx - lim)
-    return [dfx, lim, f_x_d]
+    difference = abs(dfx - lim)
+    return [lim, difference]
    
 
 
 def main():
-    e = 2.22e-16
     x = int(input("Ingrese el valor de x: "))
     fx = str(input("Ingrese la funcion: "))
-    dx = sqrt(e) * x
+    dx_value = sqrt(2.22e-16) * x
 
-    result = algorithm_1(fx, x)
-    dfx, lim, f_x_d_l = algorithm_2(result, x, dx, f, fx,e)
-    print("La derivada de la funcion es: ", dfx, "\ny la derivada calculada por el algoritmo es: ", lim)
-    print("La diferencia entre  derivada de f(x) - el limite f(x + dx)-f(x) / dx = ", f_x_d_l)
+    derivate = algorithm_1(fx, x)
+    lim, difference = algorithm_2(derivate, x, dx_value, f, fx)
+    print("La derivada de la funcion es: ", derivate, "\ny la derivada calculada por la definicion de limites es  es: ", lim)
+    print("La diferencia entre  derivada de f(x) - el limite f(x + dx)-f(x) / dx = ", difference)
 
 
 if __name__ == '__main__':
