9a5e4cd10dec90c9834c709fdae32c0688575fbf
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2022-06-06 17:08:49 -0400

    init

--- critical point type/main.py
+++ critical point type/main.py
@@ -1,8 +1,31 @@
+from sympy import *
 
 
+def functions(f: str):
+    """
+    If the input is a function, return it. Otherwise, convert it to a function
+
+    :param f: the function to be integrated
+    :type f: str
+    :return: The function itself if it is a function, otherwise it is converted to a sympy function.
+    """
+    return f if isinstance(f, Function) else sympify(f)
+
+
+def point_type(x, y, solution):
+    for point in solution:
+        pass
 
 
 def main():
-    pass
+    x,y = Symbol('x y')
+
+    function = functions("x**2 + 4*y**2 - 2*(x**2)*y + 4")
+    derivate_x = function.diff(x)
+    derivate_y = function.diff(y)
+    solution = solve((derivate_x, derivate_y), [x, y])
+    print(solution)
+
+
 if __name__ == '__main__':
     main()
