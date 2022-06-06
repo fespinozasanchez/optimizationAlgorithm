fc759a71286ff2cb221d2c160c12570f0114073f
Mario <mariocastillosan@gmail.com>
2022-06-06 17:28:54 -0400

    update

--- critical point type/main.py
+++ critical point type/main.py
@@ -17,15 +17,22 @@ def point_type(x, y, solution):
         pass
 
 
+def derivatives(f,x,y):
+    dx = diff(f, x)
+    dy = diff(f, y)
+    dx2 = diff(dx, x)
+    dy2 = diff(dy, y)
+    fxy = diff(dx, y)
+    sol = solve((dx, dy), (x, y))
+    dev=[dx,dy,dx2,dy2,fxy,sol]
+    return dev
+
 def main():
     x = Symbol('x')
     y = Symbol('y')
     f = input('Enter a function: ')
     function = functions(f)
-    derivate_x = function.diff(x)
-    derivate_y = function.diff(y)
-    solution = solve((derivate_x, derivate_y), [x, y])
-    print(solution)
+    print(derivatives(function,x,y))
 
 
 if __name__ == '__main__':
