b8d86d1db571d23d5f655f0cb4009f8f3e82cda2
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2022-06-11 17:33:42 -0400

    update
    Fix

---
+++ descending gradient with t fixed/main.py
@@ -0,0 +1,22 @@
+import sympy
+from sympy import*
+
+
+
+coord_x = float(input("(x): "))
+coord_y = float(input("(y): "))
+
+def get_Point(x,y):
+    """
+    It takes a string of comma separated numbers and returns a list of the numbers
+    
+    :param points: The points parameter is a string of comma-separated coord. pairs.
+    x and y values should be separated by a comma. For example,
+    points = (10,3)
+    :return: A list of the points
+    """
+    return  Matrix([[x], [y]])
+
+
+print(get_Point(coord_x,coord_y))
+
