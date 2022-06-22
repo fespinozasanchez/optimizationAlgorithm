4f81175acdfb2bb728c7ff7788afb03b7efff2cc
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2022-06-21 22:02:58 -0400

    hola x10? 🤷‍♀️🤦‍♂️🤦‍♀️🤷‍♀️🤦‍♂️🤷‍♀️🤦‍♂️🤷‍♀️🤦‍♂️🤷‍♀️🤦‍♂️

--- descending gradient with t fixed/main.py
+++ descending gradient with t fixed/main.py
@@ -1,7 +1,9 @@
 import sympy
 from sympy import*
+# Modulos con los que trabajaremos
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
 import numpy as np
-
 
 def functions(f: str):
     """
@@ -13,14 +15,24 @@ def functions(f: str):
     """
     return f if isinstance(f, Function) else sympify(f)
 
-
-
 def get_Symbols()->list:
     """
     It returns a list of two symbols, `x` and `y`
     :return: A list of two symbols, x and y.
     """
     return Symbol('x'), Symbol('y')
+
+
+def get_Gradient(*args: any) -> list:
+    """
+    > The function `get_Gradient` takes in two arguments, `x` and `y`, and returns the gradient of the
+    function `f` with respect to `x` and `y`
+    
+    :param : $
+    :type : any
+    :return: A Matrix of the partial derivatives of the function with respect to the variables.
+    """
+    if len(args) > 0: return Matrix([[diff(args[0], args[1])], [diff(args[0], args[2])]])
 
 
 def get_Point(points: str) -> list:
@@ -33,39 +45,36 @@ def get_Point(points: str) -> list:
     """
     return Matrix([ [float(point)] for point in points.split(',') ])
 
-def get_Derivative(f: sympy.Expr, x: sympy.Symbol, n: int) -> sympy.Expr:
+def desc_Gradient(point: list, gradient: list, t: float, n: float, *args:any) -> list:
     """
-    It takes a function, a variable, and a number, and returns the nth derivative of the function with
-    respect to the variable
-
-    :param f: sympy.Expr - the function to take the derivative of
-    :type f: sympy.Expr
-    :param x: the variable you want to differentiate with respect to
-    :type x: sympy.Symbol
-    :param n: the number of times you want to differentiate the function
-    :type n: int
-    :return: The nth derivative of f with respect to x.
+    The function takes in a point, a gradient, a step size, a tolerance, and a list of variables, and
+    returns the point at which the gradient is less than the tolerance
+    
+    :param point: list, gradient: list, t: float, n: float, *args:any
+    :type point: list
+    :param gradient: the gradient of the function
+    :type gradient: list
+    :param t: step size
+    :type t: float
+    :param n: the norm of the gradient vector
+    :type n: float
+    :param : point is the starting point
+    :type : any
+    :return: The point at which the gradient is less than n.
     """
-    return sympy.diff(f, x, n)
+    ad = []
+    z = []
+    gradient_vector = gradient.subs([(args[0], point[0]), (args[1], point[1])]).norm()
+    while  gradient_vector >= n:
+        gradient_vector = gradient.subs([(args[0], point[0]), (args[1], point[1])]).norm()
+        print(gradient_vector)
+        delta_x_n = - gradient.subs([(args[0], point[0]), (args[1], point[1])])
+        point = point + t * delta_x_n
+        ad.append(point)
+        z.append(gradient_vector)
+    print("Punto Optimo: " , point, "para el ",gradient)
+    return ad,z
 
-
-
-
-
-
-def get_Epsilon()->float:
-    """
-    The function returns the smallest positive number that can be represented by the float data type
-    :return: The smallest positive number that can be represented in the float data type.
-    """
-    return np.finfo(float).eps
-
-
-
-def Newton_method(f: sympy.Expr, x: sympy.Symbol, t: float, n: int, gradiente:sympy.matrices.dense.MutableDenseMatrix)->sympy.Expr:
-
-
-    return t*gradiente
 
 
 def main():
@@ -73,11 +82,38 @@ def main():
     It takes a function, a point, a scalar, and a number, and returns the gradient descent of the
     function at the point with the scalar and number
     """
-    e = get_Epsilon()
-    x, y = get_Symbols()
     funcion = functions(str(input("Ingrese su función: ")))
     punto = get_Point(str(input("Ingrese un Punto (x,y): ")))
+    print(punto)
+    n = float(input("Ingrese n (eta): "))
     t = float(input("Ingrese escalar t: "))
-    gradiente = get_Gradient(funcion,x,2)
-    print(type(gradiente))
+    x, y = get_Symbols()
+    gradient = get_Gradient(funcion, x, y)
+    desc, z = desc_Gradient(punto, gradient, t, n, x,y)
+    print(desc)
+    xl = []
+    yl = []
+    for e in desc:
+        xl.append(e[0])
+        yl.append(e[1])
     
+    x = np.array([xl])
+    y = np.array([yl])
+    z = np.array([z])
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111,projection='3d')
+    ax1.plot_wireframe(x, y, z, color='blue')
+    ax1.set_title("3D plot")
+    ax1.set_xlabel('x-axis')
+    ax1.set_ylabel('y-axis')
+    ax1.set_zlabel('z-axis')
+    plt.show()
+
+# Booth function
+# (x + 2*y -7 )**2 + (2*x+y-5)**2   [-10, 10]
+# Beale function
+# (1.5 - x + x*y)**2 + (2.25 - x + x*(y**2))**2 + (2.625 - x + x*(y**3))**2    [-4.5, 4.5]
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sympy
-from sympy import*
+from sympy import *
 import numpy as np
 
- 
+
 # Modulos con los que trabajaremos
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
@@ -21,7 +21,7 @@ def functions(f: str):
 
 
 ############################################################################## FUERZA BRUTA ####################################################################################
-def bruteForce(rango1,rango2, deltaXV, f, list=[]):
+def bruteForce(rango1, rango2, deltaXV, f, list=[]):
     """
     This function applies brute force to find all suboptimal solutions given a restriction and save the points evaluated and its value Z.
     :param thetaV:Refers to the θ value of this problem to evaluate.
@@ -31,7 +31,7 @@ def bruteForce(rango1,rango2, deltaXV, f, list=[]):
     """
     xl = []
     yl = []
-    z =  []
+    z = []
     try:
         for x in np.arange(rango1, rango2, deltaXV):
             for y in np.arange(rango1, rango2, deltaXV):
@@ -44,7 +44,7 @@ def bruteForce(rango1,rango2, deltaXV, f, list=[]):
                 list.append((xs, evA))
     except Exception as e:
         print(e)
-    return list,xl,yl,z
+    return list, xl, yl, z
 
 
 def maxTupla(arr):
@@ -53,9 +53,10 @@ def maxTupla(arr):
     :param arr: An array is expected  that contains tuples of  x,y points evaluated in the f(x,y) function.
     :return:Returns the max value of the function and its points (x,y).
     """
-    if len(arr)!=0: 
+    if len(arr) != 0:
         return max(arr, key=lambda x: x[1])
     return "Empty Array"
+
 
 def minTupla(arr):
     """
@@ -63,18 +64,21 @@ def minTupla(arr):
     :param arr: An array is expected  that contains tuples of  x,y points evaluated in the f(x,y) function.
     :return:Returns the max value of the function and its points (x,y).
     """
-    if len(arr)!=0: 
+    if len(arr) != 0:
         return min(arr, key=lambda x: x[1])
     return "Empty Array"
 
-def graficar3d(puntoMaximo,puntoMinimo,x,y,z):
+
+def graficar3d(puntoMaximo, puntoMinimo, x, y, z):
     fig = plt.figure()
-    ax1 = fig.add_subplot(111,projection='3d')
+    ax1 = fig.add_subplot(111, projection='3d')
     ax1.plot_wireframe(x, y, z, color='blue')
-    ax1.scatter(puntoMaximo[0][0], puntoMaximo[0][1], puntoMaximo[1], color='green', marker='o')
-    ax1.scatter(puntoMinimo[0][0], puntoMinimo[0][1], puntoMinimo[1], color='red', marker='o')
+    ax1.scatter(puntoMaximo[0][0], puntoMaximo[0][1],
+                puntoMaximo[1], color='green', marker='o')
+    ax1.scatter(puntoMinimo[0][0], puntoMinimo[0][1],
+                puntoMinimo[1], color='red', marker='o')
     ax1.scatter(1, 3, 0, color='black', marker='o')
-    ax1.set_title("3D plot")
+    ax1.set_title("Fuerza Bruta")
     ax1.set_xlabel('x-axis')
     ax1.set_ylabel('y-axis')
     ax1.set_zlabel('z-axis')
@@ -83,12 +87,13 @@ def graficar3d(puntoMaximo,puntoMinimo,x,y,z):
 
 ############################################################################## PUNTO CRITICO ####################################################################################
 
-def derivatives(f,x,y):
+
+def derivatives(f, x, y):
     """
     It takes a function (x,y)$ and returns the partial derivatives of $ with respect to $ and $,
     the second partial derivatives of $ with respect to $ and $, and the solutions to the system
     of equations $\frac{\partial f}{\partial x} = 0$ and $\frac{\partial f}{\partial y} = 0$
-    
+
     :param f: the function
     :param x: the x-coordinate of the point
     :param y: the function
@@ -103,58 +108,264 @@ def derivatives(f,x,y):
 
 
 def point_type(x, y, result):
-    derivate2_x,derivate2_y, derivate_x_y, points = result
+    derivate2_x, derivate2_y, derivate_x_y, points = result
     response = []
-    if len(points) <= 2 :
-            dic = points
-            print(dic)
-            h_x = derivate2_x.evalf(subs=dic) * derivate2_y.evalf(subs=dic) - derivate_x_y.evalf(subs=dic) **2
-            if h_x < 0: response.append(f"H(x)= {h_x} para {points} por lo tanto es: un punto silla")
-            if (h_x > 0 and (derivate2_x.evalf(subs=dic) < 0 or derivate2_y.evalf(subs=dic)<0) ):response.append( f"H(x)= {h_x} para {points} por lo tanto es: un máximo local.")
-            if (h_x > 0 and (derivate2_x.evalf(subs=dic) > 0 or derivate2_y.evalf(subs=dic)>0) ):response.append( f"H(x)= {h_x} para {points} por lo tanto es: un mínimo local.")
-            if (h_x)==0: response.append(f"No existe información suficiente sobre el punto {points}.")
+    if len(points) <= 2:
+        dic = points
+        print(dic)
+        h_x = derivate2_x.evalf(
+            subs=dic) * derivate2_y.evalf(subs=dic) - derivate_x_y.evalf(subs=dic) ** 2
+        if h_x < 0:
+            response.append(
+                f"H(x)= {h_x} para {points} por lo tanto es: un punto silla")
+        if (h_x > 0 and (derivate2_x.evalf(subs=dic) < 0 or derivate2_y.evalf(subs=dic) < 0)):
+            response.append(
+                f"H(x)= {h_x} para {points} por lo tanto es: un máximo local.")
+        if (h_x > 0 and (derivate2_x.evalf(subs=dic) > 0 or derivate2_y.evalf(subs=dic) > 0)):
+            response.append(
+                f"H(x)= {h_x} para {points} por lo tanto es: un mínimo local.")
+        if (h_x) == 0:
+            response.append(
+                f"No existe información suficiente sobre el punto {points}.")
     else:
         for point in points:
             if (type(point[1]) is not sympy.core.add.Add):
-                dic = {x:point[0],y:point[1]}
+                dic = {x: point[0], y: point[1]}
                 print(dic)
-                h_x = derivate2_x.evalf(subs=dic) * derivate2_y.evalf(subs=dic) - derivate_x_y.evalf(subs=dic) **2
-                if h_x < 0: response.append(f"H(x)= {h_x} para {point} por lo tanto es: un punto silla")
-                if (h_x > 0 and (derivate2_x.evalf(subs=dic) < 0 or derivate2_y.evalf(subs=dic)<0) ):response.append( f"H(x)= {h_x} para {point} por lo tanto es: un máximo local.")
-                if (h_x > 0 and (derivate2_x.evalf(subs=dic) > 0 or derivate2_y.evalf(subs=dic)>0) ):response.append( f"H(x)= {h_x} para {point} por lo tanto es: un mínimo local.")
-                if (h_x)==0: response.append(f"No existe información suficiente sobre el punto {point}.")
+                h_x = derivate2_x.evalf(
+                    subs=dic) * derivate2_y.evalf(subs=dic) - derivate_x_y.evalf(subs=dic) ** 2
+                if h_x < 0:
+                    response.append(
+                        f"H(x)= {h_x} para {point} por lo tanto es: un punto silla")
+                if (h_x > 0 and (derivate2_x.evalf(subs=dic) < 0 or derivate2_y.evalf(subs=dic) < 0)):
+                    response.append(
+                        f"H(x)= {h_x} para {point} por lo tanto es: un máximo local.")
+                if (h_x > 0 and (derivate2_x.evalf(subs=dic) > 0 or derivate2_y.evalf(subs=dic) > 0)):
+                    response.append(
+                        f"H(x)= {h_x} para {point} por lo tanto es: un mínimo local.")
+                if (h_x) == 0:
+                    response.append(
+                        f"No existe información suficiente sobre el punto {point}.")
     return response
 ############################################################################## PUNTO CRITICO ####################################################################################
+
+
+############################################################################## DESCENDENTE CON T FIJO ####################################################################################
+def get_Symbols() -> list:
+    """
+    It returns a list of two symbols, `x` and `y`
+    :return: A list of two symbols, x and y.
+    """
+    return Symbol('x'), Symbol('y')
+
+
+def get_Gradient(*args: any) -> list:
+    """
+    > The function `get_Gradient` takes in two arguments, `x` and `y`, and returns the gradient of the
+    function `f` with respect to `x` and `y`
+
+    :param : $
+    :type : any
+    :return: A Matrix of the partial derivatives of the function with respect to the variables.
+    """
+    if len(args) > 0:
+        return Matrix([[diff(args[0], args[1])], [diff(args[0], args[2])]])
+
+
+def get_Point(points: str) -> list:
+    """
+    It takes a string of comma separated numbers and returns a matrix of those numbers
+
+    :param points: a string of comma-separated numbers ("x,y")
+    :type points: str
+    :return: A list of lists of floats.
+    """
+    return Matrix([[float(point)] for point in points.split(',')])
+
+def desc_Gradient(point: list, gradient: list, t: float, n: float, *args:any) -> list:
+    """
+    The function takes in a point, a gradient, a step size, a tolerance, and a list of variables, and
+    returns the point at which the gradient is less than the tolerance
+    
+    :param point: list, gradient: list, t: float, n: float, *args:any
+    :type point: list
+    :param gradient: the gradient of the function
+    :type gradient: list
+    :param t: step size
+    :type t: float
+    :param n: the norm of the gradient vector
+    :type n: float
+    :param : point is the starting point
+    :type : any
+    :return: The point at which the gradient is less than n.
+    """
+    ad = []
+    z = []
+    gradient_vector = gradient.subs([(args[0], point[0]), (args[1], point[1])]).norm()
+    while  gradient_vector >= n:
+        gradient_vector = gradient.subs([(args[0], point[0]), (args[1], point[1])]).norm()
+        print(gradient_vector)
+        delta_x_n = - gradient.subs([(args[0], point[0]), (args[1], point[1])])
+        point = point + t * delta_x_n
+        ad.append(point)
+        z.append(gradient_vector)
+    print("Punto Optimo: " , point, "para el ",gradient)
+    return ad,z
+
+    
+def grafD(desc,z):
+    xl = []
+    yl = []
+    for e in desc:
+        xl.append(e[0])
+        yl.append(e[1])
+    
+    x = np.array([xl])
+    y = np.array([yl])
+    z = np.array([z])
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111,projection='3d')
+    ax1.plot_wireframe(x, y, z, color='blue')
+    ax1.set_title("Gradiente Descendente con t Fijo")
+    ax1.set_xlabel('x-axis')
+    ax1.set_ylabel('y-axis')
+    ax1.set_zlabel('z-axis')
+    plt.show()
+############################################################################## DESCENDENTE CON T FIJO ####################################################################################
+
+
+############################################################################## DESCENDENTE  Barzilai-Borwein ####################################################################################
+def derivatives2(f,x,y):
+    """
+    It takes a function (x,y)  and returns the partial derivative of x,y 
+    
+    :param f: the function
+    :param x: the x-coordinate of the point
+    :param y: the function
+    :return: The first two elements are the second derivatives of f with respect to x and y,
+    respectively. 
+    """
+    dx = diff(f, x)
+    dy = diff(f, y)
+    return Matrix([[dx],[dy]])
+
+def barz_borw(tV,xV,yV,gradientV,etaV,x,y):
+    '''
+    This function take for parameters a scale value, (x,y) coordinates, its gradient, an eta value and the x,y symbols.
+    :param tV: refers to the t value (scale value).
+    :param xV: refers to x coordinate value.
+    :param yV: refers to y coordinate value.
+    :param gradientV: refers to the gradient of the function f(x,y).
+    :param etaV: refers to eta value.
+    :param x: refers to x symbol.
+    :param y: refers to y symbol.
+    :param oGr : refers to the old gradient or the first gradient calculated before reasignating t.
+    :param nGr : refers to the new gradient value after reasignating t.
+    :param dx : referst to the Δ𝑥𝑛:=−∇𝑓(𝑥𝑛) value.
+    :param lwP :refers to the lowest part of the equation (||∇𝑓(𝑥𝑛)−∇𝑓(𝑥𝑛−1)||2)
+    :return:   Return the  Barzilai-Borwein algorithm value  on the given data.
+    '''
+    cords=Matrix([[float(xV)],[float(yV)]])
+    ad = []
+    z = []
+    while(float(gradientV.subs([(x,cords[0]),(y,cords[1])]).norm())>=etaV):
+        oGr= gradientV.subs([(x,cords[0]),(y,cords[1])])
+        dx= -tV * oGr
+        z.append(float(gradientV.subs([(x,cords[0]),(y,cords[1])]).norm()))
+        cords=cords+dx
+        nGr=gradientV.subs([(x,cords[0]),(y,cords[1])])
+        lwP = nGr - oGr
+        oGr = nGr
+        tV = dx.dot(lwP) / lwP.dot(lwP)
+        ad.append(cords)
+        
+    return cords,ad,z
+
+def grafB(desc,z):
+    xl = []
+    yl = []
+    for e in desc:
+        xl.append(e[0])
+        yl.append(e[1])
+    
+    x = np.array([xl])
+    y = np.array([yl])
+    z = np.array([z])
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111,projection='3d')
+    ax1.plot_wireframe(x, y, z, color='blue')
+    ax1.set_title("Gradiente Descendente con Barzilai-Borwein")
+    ax1.set_xlabel('x-axis')
+    ax1.set_ylabel('y-axis')
+    ax1.set_zlabel('z-axis')
+    plt.show()
+        
+
+############################################################################## DESCENDENTE  Barzilai-Borwein ####################################################################################
 
 def main1(function):
     rango1 = float(input("rango1: "))
     rango2 = float(input("rango2: "))
     deltaXV = float(input("DeltaXV: "))
-    Points, x,y,z = bruteForce(rango1,rango2, deltaXV, function)
+    Points, x, y, z = bruteForce(rango1, rango2, deltaXV, function)
     puntoMaximo = maxTupla(Points)
     puntoMinimo = minTupla(Points)
-    print("Maximo:",puntoMaximo)
-    print("Minimo:",puntoMinimo)
+    print("Maximo:", puntoMaximo)
+    print("Minimo:", puntoMinimo)
     x = np.array([x])
     y = np.array([y])
     z = np.array([z])
-    graficar3d(puntoMaximo,puntoMinimo,x,y,z)
+    graficar3d(puntoMaximo, puntoMinimo, x, y, z)
 
 
 def main2(function):
     x = Symbol('x')
     y = Symbol('y')
-    result = derivatives(function,x,y)
+    result = derivatives(function, x, y)
     print(result)
-    for i in point_type(x,y, result):
-         print(i)
+    for i in point_type(x, y, result):
+        print(i)
+
+def main3(function):
+    """
+    It takes a function, a point, a scalar, and a number, and returns the gradient descent of the
+    function at the point with the scalar and number
+    """
+    punto = get_Point(str(input("Ingrese un Punto (x,y): ")))
+    print(punto)
+    n = float(input("Ingrese n (eta): "))
+    t = float(input("Ingrese escalar t: "))
+    x, y = get_Symbols()
+    gradient = get_Gradient(function, x, y)
+    desc, z = desc_Gradient(punto, gradient, t, n, x,y)
+    print("Puntos Descendientes: ",desc)
+    grafD(desc,z)
+
+
+def main4(function):
+    x = Symbol('x')
+    y = Symbol('y')
+    tV = float(input('Enter a t value between 0-1: '))
+    eV =float( input('Enter a eta value: '))
+    xV= float(input('Enter the x  value: '))
+    yV= float(input('Enter the y  value: '))
+    gradientV = derivatives2(function,x,y)
+    cords,desc,z=barz_borw(tV,xV,yV,gradientV,eV,x,y)
+
+    print(cords)
+    print(desc)
+    grafB(desc,z)
 
 
 
- # Booth function
- # (x + 2*y -7 )**2 + (2*x+y-5)**2   [-10, 10]
- # Beale function
- # (1.5 - x + x*y)**2 + (2.25 - x + x*(y**2))**2 + (2.625 - x + x*(y**3))**2    [-4.5, 4.5]
+
+
+
+
+# Booth function
+# (x + 2*y -7 )**2 + (2*x+y-5)**2   [-10, 10]
+# Beale function
+# (1.5 - x + x*y)**2 + (2.25 - x + x*(y**2))**2 + (2.625 - x + x*(y**3))**2    [-4.5, 4.5]
 
 
 if __name__ == '__main__':
@@ -163,3 +374,7 @@ if __name__ == '__main__':
     main1(function)
     print("\nPunto Critico\n")
     main2(function)
+    print("\nGradiente Descendiente: t Fijo\n")
+    main3(function)
+    print("\nGradiente Descendiente: Barzilai-Borwein\n")
+    main4(function)
